e5b7c91485836c253fb5029479a2261cf9a5fa6c
Prajwal Tade <157635570+Prajwaltade-17@users.noreply.github.com>
2024-04-26 23:26:33 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int aai;
+	cout<<"aaigives\n"<<endl;
+	cin>>aai;
+	 int baba;
+	 cout<<"baba gives\n"<<endl;
+	 cin>>baba;
+	 
+	int sum=aai+baba;
+	cout<<"sum\n";
+	cout<<sum;
+}
